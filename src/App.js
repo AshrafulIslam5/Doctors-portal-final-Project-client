127fa0a8f5c1fefe61ffbbe10fb85747e0f4e72d
AshrafulIslam5 <ashraful.islam.mihad@gmail.com>
2022-05-11 03:46:03 +0600

    added routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { Route, Routes } from "react-router-dom";
 import About from "./Pages/About/About";
+import Appointment from "./Pages/Appointment/Appointment";
+import Contact from "./Pages/Contact/Contact";
 import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import Reviews from "./Pages/Reviews/Reviews";
 import Header from "./Pages/Shared/Header/Header";
 
 function App() {
@@ -11,6 +15,10 @@ function App() {
         <Route path="/" element={<Home></Home>} />
         <Route path="/home" element={<Home></Home>} />
         <Route path="/about" element={<About></About>} />
+        <Route path="/appointment" element={<Appointment></Appointment>} />
+        <Route path="/reviews" element={<Reviews></Reviews>} />
+        <Route path="/contact" element={<Contact></Contact>} />
+        <Route path="/login" element={<Login></Login>} />
       </Routes>
     </div>
   );
